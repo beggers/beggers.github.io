2bb0c692fdd009fe92748b8b10df04b420c4ec47
Ben Eggers <ben.eggers36@gmail.com>
2023-01-23 11:52:11 -0800

    header and title changes

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Welcome to "Ben yells at clouds"';
+export const SITE_DESCRIPTION = 'Reliability Rants';
