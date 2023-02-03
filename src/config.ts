0bbb2d8d485c7d4fbcbecc111952f4ccec2790fc
Ben Eggers <ben.eggers36@gmail.com>
2023-02-02 20:38:00 -0800

    make title logic work

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = ': Ben Eggers';
-export const SITE_DESCRIPTION = 'Reliability Rants';
+export const SITE_TITLE = 'Ben Eggers';
+export const SITE_DESCRIPTION = "Ben Eggers' digital asylum";
