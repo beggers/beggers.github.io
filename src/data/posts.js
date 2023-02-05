9a9eb1afc9d5659462ab97ae4c9cc000b9069277
Ben Eggers <ben.eggers36@gmail.com>
2023-02-05 13:14:05 -0800

    add publish tag to frontmatter

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,4 +1,7 @@
 export async function recentPosts(allPosts) {
+  allPosts = allPosts.filter((p) => {
+    return p.frontmatter.publish;
+  });
   allPosts = allPosts.sort((a, b) => {
     return new Date(b.frontmatter.published) - new Date(a.frontmatter.published);
   });
