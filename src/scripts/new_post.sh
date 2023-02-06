8e5a33a8c19e32a45c64affcdb8242ca65113340
Ben Eggers <ben.eggers36@gmail.com>
2023-02-05 16:01:14 -0800

    tweaks to new post script

--- src/scripts/new_post.sh
+++ src/scripts/new_post.sh
@@ -21,14 +21,15 @@ function main () {
   echo $SEPARATOR >> $FILE
   echo "title: $TITLE" >> $FILE
   echo "slug: $SLUG" >> $FILE
+  echo "publish: false" >> $FILE
   echo "published: '$TODAY'" >> $FILE
-  echo "updated: '$TODAY'" >> $FILE
+  echo "updated:" >> $FILE
   echo "tags: [$TAGS]" >> $FILE
   echo $SEPARATOR >> $FILE
 
   vim $FILE
 
-  git add $FILE && git commit -m 'first draft of $FILE'
+  git add $FILE && git commit -m "first draft of $FILE"
 }
 
 main
