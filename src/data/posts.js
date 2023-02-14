b4f2ce037da30daa8db2f132c8c90c1d66b388c2
Ben Eggers <ben.eggers36@gmail.com>
2023-02-14 13:28:56 -0800

    post filter (on publish) and sort

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,16 +1,22 @@
 import POSTS_ON_FRONT_PAGE from '../config.ts';
 
-export async function recentPosts(allPosts) {
-  allPosts = allPosts.filter((p) => {
+export function filterAndSort(posts) {
+  posts = posts.filter((p) => {
     return p.frontmatter.publish;
   });
-  allPosts = allPosts.sort((a, b) => {
+  posts = posts.sort((a, b) => {
     return new Date(b.frontmatter.published) - new Date(a.frontmatter.published);
   });
+  return posts;
+}
+
+export async function recentPosts(allPosts) {
+  allPosts = filterAndSort(allPosts);
   return allPosts.slice(0, POSTS_ON_FRONT_PAGE);
 }
 
 export function getTagsToPosts(allPosts) {
+  allPosts = filterAndSort(allPosts);
   var d = {};
   allPosts.forEach((post) => {
     const tags = post.frontmatter.tags;
