5eb5473c7893493146b145e0f032ac8b8461fd60
Ben Eggers <ben@beneggers.com>
2023-07-08 23:53:44 -0400

    try to inline stylesheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://beneggers.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://beneggers.com',
+  integrations: [mdx(), sitemap()],
+  build: {
+    inlineStylesheets: 'auto',
+  },
 });
