09d54c6428d46a0641803c1523c9ff09519bb8de
Ben Eggers <ben@beneggers.com>
2023-09-11 01:25:35 -0400

    cloudfront tf. this should be everything modulo bugs (still getting SSL errors)

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -7,32 +7,8 @@ resource "aws_route53_record" "main_a" {
   name    = var.domainName
   type    = "A"
   alias {
-    name                    = aws_s3_bucket_website_configuration.main.website_domain
-    zone_id                 = aws_s3_bucket.main.hosted_zone_id
+    name                    = aws_cloudfront_distribution.main.domain_name
+    zone_id                 = aws_cloudfront_distribution.main.hosted_zone_id
     evaluate_target_health  = false
   }
-}
-
-resource "aws_acm_certificate" "main" {
-  provider                  = aws.acm_provider
-  domain_name               = var.domainName
-  validation_method         = "DNS"
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate_validation" "main" {
-  provider                  = aws.acm_provider
-  certificate_arn           = aws_acm_certificate.main.arn
-  validation_record_fqdns   = [for record in aws_route53_record.acm_validation : record.fqdn]
-}
-
-resource "aws_route53_record" "acm_validation" {
-  zone_id = aws_route53_zone.main.zone_id
-  name = element(aws_acm_certificate.main.domain_validation_options.*.resource_record_name, count.index)
-  count = length(aws_acm_certificate.main.domain_validation_options)
-  type = element(aws_acm_certificate.main.domain_validation_options.*.resource_record_type, count.index)
-  records = [element(aws_acm_certificate.main.domain_validation_options.*.resource_record_value, count.index)]
-  ttl = 60
 }
