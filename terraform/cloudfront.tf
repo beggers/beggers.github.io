95d16ab25744e4efbfb4b301963968a6455552c6
Ben Eggers <ben@beneggers.com>
2023-09-11 03:01:42 -0400

    aaaa record

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -47,6 +47,8 @@ resource "aws_cloudfront_distribution" "main" {
   }
   
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = aws_acm_certificate.main.arn
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
--- terraform/dns.tf
+++ terraform/dns.tf
@@ -12,3 +12,14 @@ resource "aws_route53_record" "main_a" {
     evaluate_target_health  = false
   }
 }
+
+resource "aws_route53_record" "main_aaaa" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domainName
+  type    = "AAAA"
+  alias {
+    name                    = aws_cloudfront_distribution.main.domain_name
+    zone_id                 = aws_cloudfront_distribution.main.hosted_zone_id
+    evaluate_target_health  = false
+  }
+}
