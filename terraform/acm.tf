9b43ed4f0540d5b2cc45c9c395f57470042edd19
Ben Eggers <ben@beneggers.com>
2023-10-28 14:41:44 -0700

    wildcard ssl cert

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,24 +1,24 @@
 resource "aws_acm_certificate" "main" {
-  provider                  = aws.acm_provider
-  domain_name               = var.domainName
-  validation_method         = "DNS"
+  provider          = aws.acm_provider
+  domain_name       = var.domainName
+  validation_method = "DNS"
   lifecycle {
     create_before_destroy = true
   }
-  subject_alternative_names = [ "www.${var.domainName}" ]
+  subject_alternative_names = ["*.${var.domainName}"]
 }
 
 resource "aws_acm_certificate_validation" "main" {
-  provider                  = aws.acm_provider
-  certificate_arn           = aws_acm_certificate.main.arn
-  validation_record_fqdns   = [for record in aws_route53_record.acm_validation : record.fqdn]
+  provider                = aws.acm_provider
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
 }
 
 resource "aws_route53_record" "acm_validation" {
   zone_id = aws_route53_zone.main.zone_id
-  name = element(aws_acm_certificate.main.domain_validation_options.*.resource_record_name, count.index)
-  count = length(aws_acm_certificate.main.domain_validation_options)
-  type = element(aws_acm_certificate.main.domain_validation_options.*.resource_record_type, count.index)
+  name    = element(aws_acm_certificate.main.domain_validation_options.*.resource_record_name, count.index)
+  count   = length(aws_acm_certificate.main.domain_validation_options)
+  type    = element(aws_acm_certificate.main.domain_validation_options.*.resource_record_type, count.index)
   records = [element(aws_acm_certificate.main.domain_validation_options.*.resource_record_value, count.index)]
-  ttl = 60
+  ttl     = 60
 }
