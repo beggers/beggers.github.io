c241f938bccf0e14dc20e118a13d50741bab2f9b
Ben Eggers <ben@beneggers.com>
2023-11-02 18:34:02 -0700

    fully move cert provisioning into module, delete unused cloudfront identity

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,9 +16,3 @@ provider "aws" {
   region                   = "us-west-1"
   shared_credentials_files = ["~/.aws/credentials"]
 }
-
-provider "aws" {
-  alias                    = "acm_provider"
-  region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/credentials"]
-}
