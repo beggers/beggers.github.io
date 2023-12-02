1b452d9828654b98afcda81580687d98fa556c96
Ben Eggers <ben@beneggers.com>
2023-12-02 00:53:24 -0500

    cleanup

--- scripts/invalidate_caches.sh
+++ scripts/invalidate_caches.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd terraform/ && terraform output | grep cloudfront_distribution_id | cut -d" " -f3 | xargs -I{} -n 1 aws cloudfront create-invalidation --distribution-id={} --paths "/"
+cd terraform/ && terraform output | grep cloudfront_distribution_id | cut -d" " -f3 | xargs -I{} -n 1 sh -c '{ aws cloudfront create-invalidation --distribution-id={} --paths "/"; sleep 1; }'
