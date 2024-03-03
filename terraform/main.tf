089d35b8eaf80e178831f394e56e3228eda8461a
Ben Eggers <ben@beneggers.com>
2024-03-03 15:34:07 -0800

    kill

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,1 @@
-module "index" {
-  source = "./zone_deployment"
 
-  content_type   = "text/html"
-  domain_aliases = ["www.${var.domainName}"]
-  file           = "index.html"
-  file_directory = "../public/"
-  fqdn           = var.domainName
-  source_hash    = filemd5("../public/index.html")
-  zone_id        = aws_route53_zone.main.zone_id
-}
