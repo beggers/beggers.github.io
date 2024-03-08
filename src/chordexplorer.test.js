9448fa783800177d42ffffe69070f1f202cde5da
Ben Eggers <ben@beneggers.com>
2024-03-07 17:54:24 -0800

    property testing

--- src/chordexplorer.test.js
+++ src/chordexplorer.test.js
@@ -1,5 +1,18 @@
 import { chordDistance } from './chordexplorer';
+import fc from 'fast-check';
 
-test('basic chord distance', () => {
-  expect(chordDistance([0, 4, 7], [0, 4, 7])).toBe(0);
+test('distance from equal chords should always be zero', () => {
+  fc.assert(
+    fc.property(fc.array(fc.string()), (chord) => {
+      expect(chordDistance(chord, chord)).toBe(0);
+    })
+  );
+});
+
+test('distance should be symmetric', () => {
+  fc.assert(
+    fc.property(fc.array(fc.string()), fc.array(fc.string()), (a, b) => {
+      expect(chordDistance(a, b)).toBe(chordDistance(b, a));
+    })
+  );
 });
