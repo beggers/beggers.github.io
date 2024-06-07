1776e6fe669c4e43ebe69d8d1135eee22aae1012
Ben Eggers <ben@beneggers.com>
2024-06-07 14:34:33 -0700

    test ingest markdown

--- beneggerscom/test/ssg/test_site_generator.py
+++ beneggerscom/test/ssg/test_site_generator.py
@@ -27,11 +27,35 @@ And another
 
 
 @pytest.fixture
+def md_dir_illegal_ext(tmp_path, valid_md_file):
+    test_dir = tmp_path / "test"
+    test_dir.mkdir()
+    test_md = test_dir / "test.txt"
+    test_md.write_text(valid_md_file)
+    return test_dir
+
+
+@pytest.fixture
 def md_dir_single_file(tmp_path, valid_md_file):
     test_dir = tmp_path / "test"
     test_dir.mkdir()
     test_md = test_dir / "test.md"
     test_md.write_text(valid_md_file)
+    return test_dir
+
+
+@pytest.fixture
+def md_dir_with_subdir(tmp_path, valid_md_file):
+    test_dir = tmp_path / "test"
+    test_dir.mkdir()
+    test_md = test_dir / "test.md"
+    test_md.write_text(valid_md_file)
+
+    test_dir2 = test_dir / "test_again"
+    test_dir2.mkdir()
+    test_md2 = test_dir2 / "test_again.md"
+    test_md2.write_text(valid_md_file)
+
     return test_dir
 
 
@@ -41,3 +65,29 @@ def test_ingest_single_markdown_file(site_generator, md_dir_single_file):
     for k, v in site_generator.markdowns.items():
         assert k.endswith("test.md")
         assert v.title == "Test"
+
+
+def test_ingest_markdown_files_subdir(site_generator, md_dir_with_subdir):
+    site_generator.ingest_markdown_directory(md_dir_with_subdir)
+    assert len(site_generator.markdowns) == 2
+    assert any(
+        map(
+            lambda s: "test/test_again/test_again.md" in s,
+            site_generator.markdowns.keys(),
+        )
+    )
+
+
+def test_ingest_markdown_raises_on_double_ingest(
+    site_generator, md_dir_single_file
+):
+    site_generator.ingest_markdown_directory(md_dir_single_file)
+    with pytest.raises(ValueError):
+        site_generator.ingest_markdown_directory(md_dir_single_file)
+
+
+def test_ingest_markdown_raises_if_non_md_extension_found(
+    site_generator, md_dir_illegal_ext
+):
+    with pytest.raises(ValueError):
+        site_generator.ingest_markdown_directory(md_dir_illegal_ext)
