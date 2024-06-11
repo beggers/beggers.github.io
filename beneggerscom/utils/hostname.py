31eeab1913ecd920b45598119a509b214ebc42c9
Ben Eggers <ben@beneggers.com>
2024-06-11 10:16:58 -0700

    make types happy

--- beneggerscom/utils/hostname.py
+++ beneggerscom/utils/hostname.py
@@ -1,15 +1,15 @@
 def filename_to_url(path: str, protocol: str, hostname: str) -> str:
     if not protocol or not hostname:
         raise ValueError("Must specify protocol and hostname")
-    parts = path.split(".")
+    parts: list[str] = path.split(".")
     if len(parts) > 2:
         raise ValueError(f"Multiple periods in path? {path}")
-    path = parts[0]
-    url = path.split("/")
-    if url[-1] == "index":
-        url = url[:-1]
-    url.reverse()
-    url = ".".join([*url, hostname])
+    path_without_extension: str = parts[0]
+    fragments: list[str] = path_without_extension.split("/")
+    if fragments[-1] == "index":
+        fragments = fragments[:-1]
+    fragments.reverse()
+    url: str = ".".join([*fragments, hostname])
     if len(url) > 255:
         raise ValueError(f"URL too long. {url}")
     return protocol + "://" + url
